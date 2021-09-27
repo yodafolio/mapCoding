5ef179b215412540e4dc286e9922357a545b1cb8
yodafolio <af34532292@gmail.com>
2021-09-27 14:56:28 +0900

    marker 및 reset 기능 완료

---
+++ src/components/marker.vue
@@ -0,0 +1,20 @@
+<template>
+  <img
+    class="container-map-div-marker"
+    alt="marker"
+    src="@/source/map/marker.png"
+    :style="mData"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    mData: {
+      type: Object,
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/CheckMap.vue
+++ src/views/CheckMap.vue
@@ -7,7 +7,10 @@
       <div>img_y: {{ img_y }}</div>
       <div>elementOffsetX: {{ elementOffsetX }}</div>
       <div>elementOffsetY: {{ elementOffsetY }}</div>
+      <div>mapHeight: {{ mapHeight }}</div>
+      <div>mapWidth: {{ mapWidth }}</div>
     </div>
+
     <div id="mapBox" class="container-map">
       <div id="imgWrap" class="container-map-div">
         <img
@@ -19,18 +22,17 @@
           @mousedown="dragStart"
           @mouseup="dragStop"
           @mousemove="dragGing"
-          @contextmenu="test"
+          @contextmenu="marker"
         />
-        <img
-          class="container-map-div-marker"
-          alt="marker"
-          src="@/source/map/marker.png"
-        />
+        <template v-for="m in markers">
+          <CheckMarker :mData="m" :key="m.key" />
+        </template>
       </div>
       <img
         class="container-map-reset"
         alt="reset"
         src="@/source/map/reset.png"
+        @click="resetMarker"
       />
       <!-- <img class="" alt="Vue logo" src="../assets/logo.png" /> -->
     </div>
@@ -38,26 +40,46 @@
 </template>
 
 <script>
+import CheckMarker from "@/components/marker.vue";
 export default {
+  components: {
+    CheckMarker,
+  },
   data() {
     return {
+      markers: [],
       dragging: false,
       img_x: "no",
       img_y: "no",
       elementOffsetX: 0,
       elementOffsetY: 0,
+      mapWidth: 0,
+      mapHeight: 0,
+      tisActive: true,
     };
   },
   methods: {
-    test(e) {
-      console.log(document.querySelector("#imgWrap").clientHeight);
-      // document.querySelector("#imgWrap").clientHeight;
-      console.log("aa", e);
+    resetMarker() {
+      this.markers = [];
+    },
+    watchMap() {
+      const d = document.querySelector("#moveMap");
+      const wrap = document.querySelector("#imgWrap");
+      wrap.style.width = `${d.clientWidth}px`;
+      wrap.style.height = `${d.clientHeight}px`;
+      this.mapWidth = d.clientWidth;
+      this.mapHeight = d.clientHeight;
+    },
+    marker(e) {
+      this.markers.push({
+        left: `${e.layerX}px`,
+        top: `${e.layerY}px`,
+        key: this.markers.length,
+      });
     },
     dragStart() {
       this.dragging = true;
-      // this.img_x = this.img_y = 0;
-      const d = document.querySelector("#moveMap");
+      const d = document.querySelector("#imgWrap");
       this.elementOffsetX = this.img_x - d.offsetLeft;
       this.elementOffsetY = this.img_y - d.offsetTop;
       d.style.left = `${this.img_x - this.elementOffsetX}px`;
@@ -74,7 +96,7 @@ export default {
       let totalX = this.img_x - this.elementOffsetX;
       let totalY = this.img_y - this.elementOffsetY;
       if (this.dragging && totalX < 0 && totalY < 0) {
-        const d = document.querySelector("#moveMap");
+        const d = document.querySelector("#imgWrap");
         d.style.left = `${totalX}px`;
         d.style.top = `${totalY}px`;
       }
@@ -83,6 +105,10 @@ export default {
   mounted() {
     window.addEventListener("mouseup", this.dragStop);
     document.oncontextmenu = () => false;
+    window.addEventListener("load", this.watchMap);
+  },
+  destroyed() {
+    window.removeEventListener("load", this.watchMap);
   },
 };
 </script>
@@ -93,7 +119,6 @@ export default {
   flex-direction: column;
   align-items: center;
   position: relative;
-  border: 1px solid red;
   &-map {
     display: block;
     position: relative;
@@ -104,21 +129,24 @@ export default {
     // border: 1px solid black;
     &-div {
       display: block;
+      position: absolute;
+      z-index: 0;
+      left: -200px;
+      top: -200px;
       &-piece {
         position: absolute;
         z-index: 0;
-        left: -200px;
-        top: -200px;
+        left: 0px;
+        top: 0px;
         right: 0px;
         bottom: 0px;
       }
       &-marker {
         position: absolute;
         z-index: 2;
-        left: 0px;
-        top: 0px;
         //   border: 1px solid blue;
         cursor: pointer;
+        transform: translate(-43%, -87%);
       }
     }
     &-reset {
@@ -128,7 +156,6 @@ export default {
       top: 24px;
       cursor: pointer;
     }
-    // background-image: url("../source/map/map.png");
   }
 }
 </style>
